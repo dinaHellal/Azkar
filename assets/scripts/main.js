51562ce5de99c137958475e4ecddf58ce8a53e9f
dina hellal <dinahellal12@gmail.com>
2025-01-17 01:00:44 +0200

    Add: Morning Remembrances Section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,3 +10,19 @@ document.getElementById("dark-theme").onclick = () => {
 document.getElementById("toggle-icon").onclick = () => {
   document.getElementById("nav-menu").classList.toggle("open");
 };
+
+fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let morning = "";
+    for (let i = 0; i < data["أذكار الصباح"].length; i++) {
+      morning += `
+  
+  <li>
+  <p> <span>${data["أذكار الصباح"][i].content}</span> <span>${data["أذكار الصباح"][i].count}</span></p>
+  <p class="description">${data["أذكار الصباح"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("morning-remembrances").innerHTML = morning;
+  });
+
