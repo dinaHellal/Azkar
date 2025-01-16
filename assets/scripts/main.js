583a6c6c645d32bfab6e9754d7a2ab93111d6ca8
dina hellal <dinahellal12@gmail.com>
2025-01-17 01:43:26 +0200

    Add : Other Sections

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,3 +26,106 @@ fetch(`https://www.myjsons.com/v/b7d03829`)
     document.getElementById("morning-remembrances").innerHTML = morning;
   });
 
+fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let evening = "";
+    for (let i = 0; i < data["أذكار المساء"].length; i++) {
+      evening += `
+  
+  <li>
+  <p> <span>${data["أذكار المساء"][i].content}</span> <span>${data["أذكار المساء"][i].count}</span></p>
+  <p class="description">${data["أذكار المساء"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("evening-remembrances").innerHTML = evening;
+  });
+
+fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let afterPrayer = "";
+    for (let i = 0; i < data["أذكار بعد السلام من الصلاة المفروضة"].length; i++) {
+      afterPrayer += `
+  
+  <li>
+  <p> <span>${data["أذكار بعد السلام من الصلاة المفروضة"][i].content}</span> <span>${data["أذكار بعد السلام من الصلاة المفروضة"][i].count}</span></p>
+  <p class="description">${data["أذكار بعد السلام من الصلاة المفروضة"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("after-prayer-remembrances").innerHTML = afterPrayer;
+  });
+
+  fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let rosaries = "";
+    for (let i = 0; i < data["تسابيح"].length; i++) {
+      rosaries += `
+  
+  <li>
+  <p> <span>${data["تسابيح"][i].content}</span> <span>${data["تسابيح"][i].count}</span></p>
+  <p class="description">${data["تسابيح"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("rosaries-remembrances").innerHTML = rosaries;
+  });
+  fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let sleeping = "";
+    for (let i = 0; i < data["أذكار النوم"].length; i++) {
+      sleeping += `
+  
+  <li>
+  <p> <span>${data["أذكار النوم"][i].content}</span> <span>${data["أذكار النوم"][i].count}</span></p>
+  <p class="description">${data["أذكار النوم"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("sleeping-remembrances").innerHTML = sleeping;
+  });
+
+  fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let wakeUp = "";
+    for (let i = 0; i < data["أذكار الاستيقاظ"].length; i++) {
+      wakeUp += `
+  
+  <li>
+  <p> <span>${data["أذكار الاستيقاظ"][i].content}</span> <span>${data["أذكار الاستيقاظ"][i].count}</span></p>
+  <p class="description">${data["أذكار الاستيقاظ"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("wake-up-remembrances").innerHTML = wakeUp;
+  });
+
+  fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let quran = "";
+    for (let i = 0; i < data["أدعية قرآنية"].length; i++) {
+      quran += `
+  
+  <li>
+  <p> <span>${data["أدعية قرآنية"][i].content}</span> <span>${data["أدعية قرآنية"][i].count}</span></p>
+  <p class="description">${data["أدعية قرآنية"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("quran-supplications").innerHTML = quran;
+  });
+
+  fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((res) => res.json())
+  .then((data) => {
+    let prophets = "";
+    for (let i = 0; i < data["أدعية الأنبياء"].length; i++) {
+      prophets += `
+  
+  <li>
+  <p> <span>${data["أدعية الأنبياء"][i].content}</span> <span>${data["أدعية الأنبياء"][i].count}</span></p>
+  <p class="description">${data["أدعية الأنبياء"][i].description}</p>
+  </li>`;
+    }
+    document.getElementById("prophets-supplications").innerHTML = prophets;
+  });
